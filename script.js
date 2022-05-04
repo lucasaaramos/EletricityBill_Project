73bf4bff16be0fe24e29ece8ea129fc7902e20e6
ramoslucas <lucasalbuquerque5412@gmail.com>
2022-05-04 19:39:17 +0100

    created calc and updated html

--- script.js
+++ script.js
@@ -12,29 +12,34 @@ window.onload = function() {
         end_loader()
     }, 500)
 
-    const loanForm = document.getElementById('calculate-loan-form')
+    const loanForm = document.getElementById('calculate-bill')
     loanForm.addEventListener('submit', function(e) {
         e.preventDefault()
         start_loader();
-        const principalAmount = document.getElementById('loan-amount').value;
-        const interest = document.getElementById('loan-interest').value;
-        const PayableYears = document.getElementById('loan-years').value;
+        const unitsQtd = document.getElementById('units-amount').value;
+        const daysQtd = document.getElementById('billing-period').value;
+        
         var monthly = 0,
-            pmt = 0,
+
+
+            vat = 13.5/100,
+		rate = 0.04,
             total = 0,
-            totalInterest = 0,
-            monthlyInterest = 0;
-        monthlyInterest = (parseFloat(principalAmount) * (parseFloat(interest) / 100)) / 12;
-        pmt = parseFloat(monthlyInterest) / (1 - Math.pow(1 + ((parseFloat(interest) / 100) / 12), -12 * parseFloat(PayableYears)));
-        total = parseFloat(pmt) * Math.floor(parseFloat(PayableYears) * 12);
-        totalInterest = parseFloat(total) - parseFloat(principalAmount);
-        setTimeout(() => {
-            document.getElementById('principal').textContent = parseFloat(principalAmount).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 })
-            document.getElementById('annual-interest').textContent = parseFloat(interest).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 }) + "%";
-            document.getElementById('loan-terms').textContent = parseFloat(PayableYears).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 }) + (PayableYears > 1 ? " Yrs." : "")
-            document.getElementById('monthly-pay').textContent = parseFloat(pmt).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 })
-            document.getElementById('total-pay').textContent = parseFloat(total).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 })
-            document.getElementById('total-interest').textContent = parseFloat(totalInterest).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 })
+            totalWithoutVar = 0,
+            totalWithVar = 0;
+        
+totalWithoutVar = (parseFloat(unitsQtd) * (parseFloat(daysQtd) * parseFloat(rate));
+
+totalWithVar = (parseFloat(unitsQtd) * (parseFloat(daysQtd) * parseFloat(rate)) * (parseFloat(vat);
+
+
+
+                setTimeout(() => {
+            document.getElementById('units').textContent = parseFloat(unitsQtd).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 })
+            document.getElementById('bill-days').textContent = parseFloat(daysQtd).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 });
+            
+            document.getElementById('amount-without-vat').textContent = parseFloat(totalWithoutVar).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 })
+            document.getElementById('total-pay').textContent = parseFloat(totalWithVar).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 })
             document.getElementById('result').style.display = 'table';
             document.getElementById('reset-btn').style.display = 'block';
             end_loader()
@@ -44,12 +49,10 @@ window.onload = function() {
     loanForm.addEventListener('reset', function(e) {
         start_loader();
         setTimeout(() => {
-            document.getElementById('principal').textContent = ""
-            document.getElementById('annual-interest').textContent = ""
-            document.getElementById('loan-terms').textContent = ""
-            document.getElementById('monthly-pay').textContent = ""
+            document.getElementById('units').textContent = ""
+            document.getElementById('bill-days').textContent = ""
+            document.getElementById('amount-without-vat').textContent = ""
             document.getElementById('total-pay').textContent = ""
-            document.getElementById('total-interest').textContent = ""
             document.getElementById('result').style.display = 'none';
             document.getElementById('reset-btn').style.display = 'none';
             end_loader()
