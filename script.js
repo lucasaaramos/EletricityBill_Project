ee4ca6969370ba21bdd7346fcf5b57fbded9585a
Lucas Albuquerque Ramos <65198672+lucasaaramos@users.noreply.github.com>
2022-05-05 13:12:00 +0100

    creating calculation using assignment data

--- script.js
+++ script.js
@@ -18,23 +18,30 @@ window.onload = function() {
         start_loader();
         const principalAmount = document.getElementById('loan-amount').value;
         const interest = document.getElementById('loan-interest').value;
-        const PayableYears = document.getElementById('loan-years').value;
-        var monthly = 0,
-            pmt = 0,
-            total = 0,
-            totalInterest = 0,
-            monthlyInterest = 0;
-        monthlyInterest = (parseFloat(principalAmount) * (parseFloat(interest) / 100)) / 12;
-        pmt = parseFloat(monthlyInterest) / (1 - Math.pow(1 + ((parseFloat(interest) / 100) / 12), -12 * parseFloat(PayableYears)));
-        total = parseFloat(pmt) * Math.floor(parseFloat(PayableYears) * 12);
-        totalInterest = parseFloat(total) - parseFloat(principalAmount);
+        
+        var total = 0,
+            vat = 0.135,
+            rate = 0.20,
+            charge = 0.04,
+            amountWithoutVat = 0,
+            amountWithVat = 0;
+            
+            
+
+          
+            amountWithoutVat = (parseFloat(principalAmount) * parseFloat(rate)) + (parseFloat(interest) * parseFloat(charge));
+        
+            amountWithVat = parseFloat(amountWithoutVat) + (parseFloat(amountWithoutVat) * parseFloat(vat));
+
+        
         setTimeout(() => {
             document.getElementById('principal').textContent = parseFloat(principalAmount).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 })
-            document.getElementById('annual-interest').textContent = parseFloat(interest).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 }) + "%";
-            document.getElementById('loan-terms').textContent = parseFloat(PayableYears).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 }) + (PayableYears > 1 ? " Yrs." : "")
-            document.getElementById('monthly-pay').textContent = parseFloat(pmt).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 })
-            document.getElementById('total-pay').textContent = parseFloat(total).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 })
-            document.getElementById('total-interest').textContent = parseFloat(totalInterest).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 })
+            document.getElementById('annual-interest').textContent = parseFloat(interest).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 });
+            
+            
+            document.getElementById('total-pay').textContent = parseFloat(amountWithoutVat).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 })
+            document.getElementById('total-interest').textContent = parseFloat(amountWithVat).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 })
+            
             document.getElementById('result').style.display = 'table';
             document.getElementById('reset-btn').style.display = 'block';
             end_loader()
@@ -46,8 +53,6 @@ window.onload = function() {
         setTimeout(() => {
             document.getElementById('principal').textContent = ""
             document.getElementById('annual-interest').textContent = ""
-            document.getElementById('loan-terms').textContent = ""
-            document.getElementById('monthly-pay').textContent = ""
             document.getElementById('total-pay').textContent = ""
             document.getElementById('total-interest').textContent = ""
             document.getElementById('result').style.display = 'none';
@@ -56,3 +61,4 @@ window.onload = function() {
         }, 500)
     })
 }
+
