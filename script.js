5856a071511a62693de9d786a9e532b7a7df28d6
ramoslucas <lucasalbuquerque5412@gmail.com>
2022-05-05 12:58:17 +0100

    calculation

--- script.js
+++ script.js
@@ -12,31 +12,36 @@ window.onload = function() {
         end_loader()
     }, 500)
 
-    const loanForm = document.getElementById('calculate-bill')
+    const loanForm = document.getElementById('calculate-loan-form')
     loanForm.addEventListener('submit', function(e) {
         e.preventDefault()
         start_loader();
-        const unitsQtd = document.getElementById('units-amount').value;
-        const daysQtd = document.getElementById('billing-period').value;
+        const principalAmount = document.getElementById('loan-amount').value;
+        const interest = document.getElementById('loan-interest').value;
         
-        var vat = 13.5/100,
-		    rate = 0.04,
-            total = 0,
-            totalWithoutVar = 0,
-            totalWithVar = 0;
-        
-totalWithoutVar = (parseFloat(unitsQtd) * parseFloat(daysQtd) * parseFloat(rate));
-
-totalWithVar = (parseFloat(unitsQtd) * (parseFloat(daysQtd) * parseFloat(rate)) * parseFloat(vat));
-
-
-
-                setTimeout(() => {
-            document.getElementById('units').textContent = parseFloat(unitsQtd).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 })
-            document.getElementById('bill-days').textContent = parseFloat(daysQtd).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 })
+        var total = 0,
+            vat = 0.135,
+            rate = 0.20,
+            charge = 0.04,
+            amountWithoutVat = 0,
+            amountWithVat = 0;
             
-            document.getElementById('amount-without-vat').textContent = parseFloat(totalWithoutVar).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 })
-            document.getElementById('total-pay').textContent = parseFloat(totalWithVar).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 })
+            
+
+          
+            amountWithoutVat = (parseFloat(principalAmount) * parseFloat(rate)) + (parseFloat(interest) * parseFloat(charge));
+        
+            amountWithVat = parseFloat(amountWithoutVat) + (parseFloat(amountWithoutVat) * parseFloat(vat));
+
+        
+        setTimeout(() => {
+            document.getElementById('principal').textContent = parseFloat(principalAmount).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 })
+            document.getElementById('annual-interest').textContent = parseFloat(interest).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 });
+            
+            
+            document.getElementById('total-pay').textContent = parseFloat(amountWithoutVat).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 })
+            document.getElementById('total-interest').textContent = parseFloat(amountWithVat).toLocaleString("en-US", { style: "decimal", maximumFractionDigits: 2 })
+            
             document.getElementById('result').style.display = 'table';
             document.getElementById('reset-btn').style.display = 'block';
             end_loader()
@@ -46,13 +51,14 @@ totalWithVar = (parseFloat(unitsQtd) * (parseFloat(daysQtd) * parseFloat(rate)) 
     loanForm.addEventListener('reset', function(e) {
         start_loader();
         setTimeout(() => {
-            document.getElementById('units').textContent = ""
-            document.getElementById('bill-days').textContent = ""
-            document.getElementById('amount-without-vat').textContent = ""
+            document.getElementById('principal').textContent = ""
+            document.getElementById('annual-interest').textContent = ""
             document.getElementById('total-pay').textContent = ""
+            document.getElementById('total-interest').textContent = ""
             document.getElementById('result').style.display = 'none';
             document.getElementById('reset-btn').style.display = 'none';
             end_loader()
         }, 500)
     })
 }
+
